28d565c9eeb64da2f201448a41abc575eed81104
SezimKubandykova <k.sezim8@gmail.com>
2022-09-21 15:35:57 +0600

    DZ_3

---
+++ js/app.js
@@ -0,0 +1,78 @@
+const tabs = document.querySelectorAll('.tabheader__item')
+const tabsParent = document.querySelector('.tabheader__items')
+const tabContent = document.querySelectorAll('.tabcontent')
+
+const hideTabContent = () => {
+    tabContent.forEach((item) => {
+        item.style.display = 'none' 
+    })
+    tabs.forEach((item) => {
+        item.classList.remove('tabheader__item_active')
+    })
+}
+
+const showTabContent = (i = 0) => {
+    tabContent[i].style.display = 'block'
+    tabs[i].classList.add('tabheader__item_active')
+}
+
+hideTabContent()
+showTabContent()
+
+tabsParent.addEventListener('click', (event) => {
+    const target = event.target
+    if (target.classList.contains('tabheader__item')) {
+        tabs.forEach((item, i) => {
+            if (target === item) {
+                hideTabContent()
+                showTabContent(i)
+            }
+        })
+    }
+})
+
+let i = 0
+const interval = () => {
+    i++
+    if(i > 3) {
+        i = 0
+    }
+    hideTabContent()
+    showTabContent(i)
+}
+setInterval (interval, 1800)
+
+const modal = document.querySelector('.modal')
+const modalTrigger = document.querySelector('.btn_white')
+const closeModalBtn = document.querySelector('.modal__close')
+
+const openModal = () => {
+    modal.classList.add('show')
+    modal.classList.remove('hide')
+    document.body.style.overflow = 'hidden'
+}
+
+modalTrigger.addEventListener('click', openModal)
+
+const closeModal = () => {
+    modal.classList.add('hide')
+    modal.classList.remove('show')
+    document.body.style.overflow = ''
+}
+closeModalBtn.addEventListener('click', closeModal)
+
+modal.addEventListener('click', (event) => {
+    if (event.target == modal) {
+        closeModal()
+    }
+})
+
+const openModalScroll = () => {
+    const page = document.documentElement;
+    if (page.scrollTop + page.clientHeight >= page.scrollHeight) {
+        openModal()
+        window.removeEventListener("scroll", openModalScroll)
+    }
+}
+window.addEventListener("scroll", openModalScroll)
+const modalTimeout = setTimeout(openModal, 50000)
